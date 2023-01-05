aa5c06bbe131313514c081d0c0c0c1a6f324d32c
Archanapudayan <atchanaudayan64534@gmail.com>
2023-01-05 11:00:28 +0530

    productentry

---
+++ src/app/productentry/productentry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productentry',
+  templateUrl: './productentry.component.html',
+  styleUrls: ['./productentry.component.css']
+})
+export class ProductentryComponent {
+
+}
