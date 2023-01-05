6c29709bf8e6fe3a570db8b1a9db2fe2118b8ba0
Archanapudayan <atchanaudayan64534@gmail.com>
2023-01-05 12:26:09 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductentryComponent } from './productentry/productentry.component';
+import { ViewproductsComponent } from './viewproducts/viewproducts.component';
 
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ProductentryComponent
+  },
+  {
+    path:"view",
+    component:ViewproductsComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    ProductentryComponent
+    ProductentryComponent,
+    ViewproductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
